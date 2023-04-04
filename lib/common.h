92cd518899724c7281bec42cf68bdc43be0717f1
Kuiisy <83209973+njhayy@users.noreply.github.com>
2023-04-04 20:49:21 +0800

    Add files via upload

---
+++ lib/common.h
@@ -0,0 +1,21 @@
+#ifndef __COMMON__H_
+#define __COMMON__H_
+
+#include <arpa/inet.h>
+#include <error.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define SERV_PORT 43211
+#define BUFFER_SIZE 4096
+#define MAXLINE 4096
+
+#endif
